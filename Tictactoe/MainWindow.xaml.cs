a62cd171cd88aa48d375ddd5156480ab2de0e915
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-22 22:17:08 +0200

    [~] Replaced all strings "X" and "O" to png and changed logic in CheckIfPlayerWon, NewGame(), Button_Click for new update, add new method CheckLine()

--- Tictactoe/MainWindow.xaml.cs
+++ Tictactoe/MainWindow.xaml.cs
@@ -16,12 +16,10 @@ using System.Windows.Shapes;
 
 namespace Tictactoe
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public bool IsPlayer1Turn { get; set; }
+		public bool LastPlayer1Turn { get; set; }
 		public int Counter { get; set; }
 		public int Player1Win { get; set; }
 		public int Player2Win { get; set; }
@@ -33,10 +31,9 @@ namespace Tictactoe
 			NewGame();
 		}
 
-
 		public void NewGame()
 		{
-			IsPlayer1Turn = true;
+			IsPlayer1Turn = !LastPlayer1Turn; 
 			Counter = 0;
 
 			if (Player1Win == 3 || Player2Win == 3)
@@ -46,43 +43,40 @@ namespace Tictactoe
 				Draw = 0;
 			}
 
+			foreach (Button btn in new[] { Button_0_0, Button_1_0, Button_2_0, Button_0_1, Button_1_1, Button_2_1, Button_0_2, Button_1_2, Button_2_2 })
+			{
+				btn.Content = string.Empty;
+				btn.Tag = null; 
+				btn.Background = Brushes.White;
+			}
 
-			Button_0_0.Content = string.Empty;
-			Button_1_0.Content = string.Empty;
-			Button_2_0.Content = string.Empty;
-			Button_0_1.Content = string.Empty;
-			Button_1_1.Content = string.Empty;
-			Button_2_1.Content = string.Empty;
-			Button_0_2.Content = string.Empty;
-			Button_1_2.Content = string.Empty;
-			Button_2_2.Content = string.Empty;
-
-			Player1.Content = "Player 1 score: " +Player1Win;
+			Player1.Content = "Player 1 score: " + Player1Win;
 			DrawTheGame.Content = "Draw: " + Draw;
 			Player2.Content = "Player 2 score: " + Player2Win;
-
-			Button_0_0.Background = Brushes.White;
-			Button_1_0.Background = Brushes.White;
-			Button_2_0.Background = Brushes.White;
-			Button_0_1.Background = Brushes.White;
-			Button_1_1.Background = Brushes.White;
-			Button_2_1.Background = Brushes.White;
-			Button_0_2.Background = Brushes.White;
-			Button_1_2.Background = Brushes.White;
-			Button_2_2.Background = Brushes.White;
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			if (!(sender is Button button) || !string.IsNullOrEmpty(button.Content.ToString()))
+			if (!(sender is Button button) || button.Tag != null)
 				return;
 
-			button.Content = IsPlayer1Turn ? "O" : "X";
+			string imageName = IsPlayer1Turn ? "O" : "X";
+
+			button.Content = new Image
+			{
+				Source = new BitmapImage(new Uri($"C:/Users/ROG/source/repos/Tictactoe/Tictactoe/Photos/{imageName}.png", UriKind.Absolute))
+			};
+
+			button.Tag = imageName; 
+
+			IsPlayer1Turn = !IsPlayer1Turn;
+			LastPlayer1Turn = IsPlayer1Turn; 
+
 			Counter++;
 
 			if (CheckIfPlayerWon())
 			{
-				if (IsPlayer1Turn)
+				if (!IsPlayer1Turn)
 				{
 					Player1Win++;
 					Player1.Content = "Player 1 score a point!";
@@ -106,7 +100,6 @@ namespace Tictactoe
 				Counter = 9;
 
 				PleaseLate();
-
 			}
 			else if (Counter == 9)
 			{
@@ -123,11 +116,6 @@ namespace Tictactoe
 
 				PleaseLate();
 			}
-			else
-			{
-				IsPlayer1Turn ^= true;
-			}
-
 		}
 
 		private void PleaseLate()
@@ -140,67 +128,34 @@ namespace Tictactoe
 		}
 		private bool CheckIfPlayerWon()
 		{
-			if (Button_0_0.Content == Button_0_1.Content && Button_0_1.Content == Button_0_2.Content && Button_0_0.Content != string.Empty)
+			if (CheckLine(Button_0_0, Button_0_1, Button_0_2) ||
+				CheckLine(Button_1_0, Button_1_1, Button_1_2) ||
+				CheckLine(Button_2_0, Button_2_1, Button_2_2) ||
+				CheckLine(Button_0_0, Button_1_0, Button_2_0) ||
+				CheckLine(Button_0_1, Button_1_1, Button_2_1) ||
+				CheckLine(Button_0_2, Button_1_2, Button_2_2) ||
+				CheckLine(Button_0_0, Button_1_1, Button_2_2) ||
+				CheckLine(Button_0_2, Button_1_1, Button_2_0))
 			{
-				Button_0_0.Background = Brushes.Green;
-				Button_0_1.Background = Brushes.Green;
-				Button_0_2.Background = Brushes.Green;
 				return true;
 			}
-			if (Button_1_0.Content == Button_1_1.Content && Button_1_1.Content == Button_1_2.Content && Button_1_0.Content != string.Empty)
-			{
-				Button_1_0.Background = Brushes.Green;
-				Button_1_1.Background = Brushes.Green;
-				Button_1_2.Background = Brushes.Green;
-				return true;
-			}
-			if (Button_2_0.Content == Button_2_1.Content && Button_2_1.Content == Button_2_2.Content && Button_2_0.Content != string.Empty)
-			{
-				Button_2_0.Background = Brushes.Green;
-				Button_2_1.Background = Brushes.Green;
-				Button_2_2.Background = Brushes.Green;
-				return true;
-			}
-
-			if (Button_0_0.Content == Button_1_0.Content && Button_1_0.Content == Button_2_0.Content && Button_0_0.Content != string.Empty)
-			{
-				Button_0_0.Background = Brushes.Green;
-				Button_1_0.Background = Brushes.Green;
-				Button_2_0.Background = Brushes.Green;
-				return true;
-			}
-			if (Button_0_1.Content == Button_1_1.Content && Button_1_1.Content == Button_2_1.Content && Button_0_1.Content != string.Empty)
-			{
-				Button_0_1.Background = Brushes.Green;
-				Button_1_1.Background = Brushes.Green;
-				Button_2_1.Background = Brushes.Green;
-				return true;
-			}
-			if (Button_0_2.Content == Button_1_2.Content && Button_1_2.Content == Button_2_2.Content && Button_0_2.Content != string.Empty)
-			{
-				Button_0_2.Background = Brushes.Green;
-				Button_1_2.Background = Brushes.Green;
-				Button_2_2.Background = Brushes.Green;
-				return true;
-			}
-
-			if (Button_0_0.Content == Button_1_1.Content && Button_1_1.Content == Button_2_2.Content && Button_0_0.Content != string.Empty)
-			{
-				Button_0_0.Background = Brushes.Green;
-				Button_1_1.Background = Brushes.Green;
-				Button_2_2.Background = Brushes.Green;
-				return true;
-			}
-			if (Button_0_2.Content == Button_1_1.Content && Button_1_1.Content == Button_2_0.Content && Button_0_2.Content != string.Empty)
-			{
-				Button_0_2.Background = Brushes.Green;
-				Button_1_1.Background = Brushes.Green;
-				Button_2_0.Background = Brushes.Green;
-				return true;
-			}
-
 			return false;
+		}
 
+		private bool CheckLine(Button b1, Button b2, Button b3)
+		{
+			string tag1 = b1.Tag as string;
+			string tag2 = b2.Tag as string;
+			string tag3 = b3.Tag as string;
+
+			if (!string.IsNullOrEmpty(tag1) && tag1 == tag2 && tag2 == tag3)
+			{
+				b1.Background = Brushes.Green;
+				b2.Background = Brushes.Green;
+				b3.Background = Brushes.Green;
+				return true;
+			}
+			return false;
 		}
 	} 
 } 
